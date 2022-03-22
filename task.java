5ffdb18e026433dded5a2a716b76707195ff4133
wafi pandega <degaforphone@gmail.com>
2022-03-22 01:06:49 -0700

    Task 1

---
+++ task.java
@@ -0,0 +1,34 @@
+
+// Nomor 2
+public class kelipatan {
+  // kelipatan 3
+n1=3
+for (int i = 0 ; i <= 100; i++) {
+  if (i%n1==0) {
+    system.out.print(i)
+  }
+}
+// Kelipatan 5
+n2=5
+for (int i = 0 ; i <= 100; i++) {
+  if (i%n1==0) {
+    system.out.print(i)
+  }
+}
+}
+
+// Nomor 1
+public class biodata {
+  firstName = "Wafi"
+  lastName = "Pandega"
+  birthPlace = "Bogor"
+  birthYear = "1997"
+  progLangFav = "php"
+
+system.out.print("Nama Saya" + firstName + lastName)
+system.out.print("Saya Lahir di" + birthPlace)
+system.out.print("Saya lahor di tahun" + birthYear)
+system.out.print("Bahasa pemograman favorit saya adalah" + progLangFav)
+}
+
+
